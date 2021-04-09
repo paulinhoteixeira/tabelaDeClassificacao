ac328fa95b2bb5d68cf8c8471a112630dcb2e36e
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-04-08 21:59:51 -0300

    função para exibir os jogadores na tela pelo javascript de forma dinamica

--- script.js
+++ script.js
@@ -14,9 +14,28 @@ let rafa = {
     pontos: 0
 }
 
+console.log(rafa)
  rafa.pontos = calculaPontos(rafa)
+ paulo.pontos = calculaPontos(paulo)
 
 function calculaPontos(jogador) {
     var pontos = (jogador.vitorias * 3) + jogador.empates
     return pontos
+}
+
+var jogadores = [rafa, paulo]
+
+exibirJogadoresNaTela(jogadores)
+
+function exibirJogadoresNaTela(jogadores) {
+    var html = ""
+    for (var i = 0; i < jogadores.length; i++){
+        html += "<tr><td>" + jogadores[i].nome + "</td>"
+        html += "<td>" + jogadores[i].vitorias + "</td>"
+        html += "<td>" + jogadores[i].empates + "</td>"
+        html += "<td>" + jogadores[i].derrotas + "</td>"
+        html += "<td>" + jogadores[i].pontos + "</td></tr>"
+    }
+    var tabelaJogadores = document.getElementById("tabelaJogadores")
+    tabelaJogadores.innerHTML = html 
 }

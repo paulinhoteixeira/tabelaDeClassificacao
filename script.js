dfb6c842d27f860859fac2451c1a5bc88fa88f44
Paulo Teixeira <pauloteixeira.ti@hotmail.com>
2021-04-08 22:31:52 -0300

    adicionados os botões e funções para adicionar vitoria, empate e derrota

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let rafa = {
     pontos: 0
 }
 
-console.log(rafa)
+
  rafa.pontos = calculaPontos(rafa)
  paulo.pontos = calculaPontos(paulo)
 
@@ -34,8 +34,32 @@ function exibirJogadoresNaTela(jogadores) {
         html += "<td>" + jogadores[i].vitorias + "</td>"
         html += "<td>" + jogadores[i].empates + "</td>"
         html += "<td>" + jogadores[i].derrotas + "</td>"
-        html += "<td>" + jogadores[i].pontos + "</td></tr>"
+        html += "<td>" + jogadores[i].pontos + "</td>"
+        html += "<td><button onClick='adicionarVitoria(" + i + ")'>Vitória</button></td>"
+        html += "<td><button onClick='adicionarEmpate(" + i + ")'>Empate</button></td>"
+        html += "<td><button onClick='adicionarDerrota(" + i + ")'>Derrota</button></td></tr>"
     }
     var tabelaJogadores = document.getElementById("tabelaJogadores")
     tabelaJogadores.innerHTML = html 
+}
+
+function adicionarVitoria(i){
+    var jogador = jogadores[i]
+    jogador.vitorias++
+    jogador.pontos = calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarEmpate(i){
+    var jogador = jogadores[i]
+    jogador.empates++
+    jogador.pontos = calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
+}
+
+function adicionarDerrota(i){
+    var jogador = jogadores[i]
+    jogador.derrotas++
+    jogador.pontos = calculaPontos(jogador)
+    exibirJogadoresNaTela(jogadores)
 }
